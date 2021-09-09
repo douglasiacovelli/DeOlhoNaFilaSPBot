ed7c4c2ad8be28d95d2b72c861323b3b967df930
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2021-09-09 08:50:23 -0300

    fix another migration

--- db/migrate/20210907030712_fix_column_type_on_chat_subscription.rb
+++ db/migrate/20210907030712_fix_column_type_on_chat_subscription.rb
@@ -1,5 +1,8 @@
 class FixColumnTypeOnChatSubscription < ActiveRecord::Migration[6.1]
   def change
-    change_column :chat_subscriptions, :health_center_id, 'int USING CAST(health_center_id AS integer)', references: :health_centers
+    change_column :chat_subscriptions,
+                  :health_center_id,
+                  'integer USING CAST(health_center_id AS integer)',
+                  references: :health_centers
   end
 end
--- db/migrate/20210907031233_make_chat_id_a_reference_on_chat_subscriptions.rb
+++ db/migrate/20210907031233_make_chat_id_a_reference_on_chat_subscriptions.rb
@@ -1,6 +1,11 @@
 class MakeChatIdAReferenceOnChatSubscriptions < ActiveRecord::Migration[6.1]
   def change
-    change_column :chat_subscriptions, :chat_id, :int, references: :chats
-    remove_column :chat_subscriptions, :active
+    change_table :chat_subscriptions, bulk: true do |t|
+      t.change_column :chat_subscriptions,
+                      :chat_id,
+                      'integer USING CAST(chat_id AS integer)',
+                      references: :chats
+      t.remove_column :chat_subscriptions, :active, :boolean
+    end
   end
 end
