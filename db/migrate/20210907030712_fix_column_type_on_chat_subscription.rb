b568ff84469356bb9c8617a044129065c8adc6f4
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2021-09-09 08:43:00 -0300

    fix migration

--- db/migrate/20210907030712_fix_column_type_on_chat_subscription.rb
+++ db/migrate/20210907030712_fix_column_type_on_chat_subscription.rb
@@ -1,5 +1,5 @@
 class FixColumnTypeOnChatSubscription < ActiveRecord::Migration[6.1]
   def change
-    change_column :chat_subscriptions, :health_center_id, :int, references: :health_centers
+    change_column :chat_subscriptions, :health_center_id, 'int USING CAST(health_center_id AS integer)', references: :health_centers
   end
 end
